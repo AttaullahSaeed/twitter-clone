61d97808ff68869fa6d6ddd046fcd59f3d101d9a
Atta Ullah Saeed <attaullahsaeed77@gmail.com>
2020-10-19 18:56:08 +0500

    Updates

---
+++ precache-manifest.72dfcdafd7a824b2f655dbff2742715d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90fe836156dcb1e177feef7d12638d77",
+    "url": "/twitter-clone/index.html"
+  },
+  {
+    "revision": "aa71a9a0dba2997c0c3d",
+    "url": "/twitter-clone/static/css/main.433d8318.chunk.css"
+  },
+  {
+    "revision": "7b438ac0094fe05c835f",
+    "url": "/twitter-clone/static/js/2.6f235fc6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/twitter-clone/static/js/2.6f235fc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa71a9a0dba2997c0c3d",
+    "url": "/twitter-clone/static/js/main.d2080020.chunk.js"
+  },
+  {
+    "revision": "4ea1c8ca6c4e7890228d",
+    "url": "/twitter-clone/static/js/runtime-main.a74d6e56.js"
+  }
+]);
